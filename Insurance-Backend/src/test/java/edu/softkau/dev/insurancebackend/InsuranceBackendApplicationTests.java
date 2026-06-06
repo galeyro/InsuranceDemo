7aaa9e7ba2c66d00cf621cbf68bdeb2fcd9eb847
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2026-06-06 15:37:20 -0500

    build: estructura de carpetas para backend, frontend, manifiestos k8s y dockerfiles

---
+++ Insurance-Backend/src/test/java/edu/softkau/dev/insurancebackend/InsuranceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.softkau.dev.insurancebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
