88403e243bfbe9c50867e2c48dc59efdb4dea123
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2026-06-08 12:40:21 -0500

    feat: enums para el domain de policy

---
+++ Insurance-Backend/src/main/java/edu/softkau/dev/insurancebackend/policy/domain/model/Branch.java
@@ -0,0 +1,8 @@
+package edu.softkau.dev.insurancebackend.policy.domain.model;
+
+public enum Branch {
+    AUTO,
+    LIFE,
+    HOME,
+    HEALTH
+}
---
+++ Insurance-Backend/src/main/java/edu/softkau/dev/insurancebackend/policy/domain/model/PolicyStatus.java
@@ -0,0 +1,9 @@
+package edu.softkau.dev.insurancebackend.policy.domain.model;
+
+public enum PolicyStatus {
+    QUOTED,
+    ISSUED,
+    ACTIVE,
+    SUSPENDED,
+    CANCELLED
+}
